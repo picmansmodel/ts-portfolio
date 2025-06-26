7d33d83dc57fc6368f388e5b8b3d022d2fbeb044
Theresa Schlag <theresa.schlag@gmail.com>
2025-06-26 22:18:53 +0200

    > fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import CircularText from './CircularText';
 
-import { motion } from 'framer-motion';
-import { useEffect, useRef, useState, useMemo } from 'react';
+import { useEffect, useRef, useMemo } from 'react';
 
 
 
